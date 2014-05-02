a9cbf0503d3d52d0ebe6b4dfa525296eb6782a08
Bruce Park <bruce@binarywebpark.com>
2014-05-01 21:19:52 -0700

    bower build pack

--- config/application.rb
+++ config/application.rb
@@ -60,5 +60,17 @@ module RailsBackboneWizard
     config.assets.version = '1.0'
 
     config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
+
+    # We don't want the default of everything that isn't js or css, because it pulls too many things in
+    config.assets.precompile.shift
+
+    # Explicitly register the extensions we are interested in compiling
+    config.assets.precompile.push(Proc.new do |path|
+      File.extname(path).in? [
+        '.html', '.erb', '.haml',                 # Templates
+        '.png',  '.gif', '.jpg', '.jpeg', '.svg', # Images
+        '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
+      ]
+    end)
   end
 end
